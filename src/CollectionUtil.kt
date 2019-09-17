8477e79172a31b6ad461cb993e7c3d8f2a1be33c
LENOVO <464267716@qq.com>
2019-09-17 13:54:00 +0800

    提交kotlin的代码

--- src/CollectionUtil.kt
+++ src/CollectionUtil.kt
@@ -22,6 +22,7 @@ fun testList() {
     var plainSet= setOf<String>("oneSet","twoSet")
     println(plainSet)
     val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+    println(numbersMap)
 }
 
 
